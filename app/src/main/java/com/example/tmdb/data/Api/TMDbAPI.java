9bc9c641c82bc3edfceccf7713ba1166bc6f129e
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-03-27 10:30:38 +0100

    Api Connectie

---
+++ app/src/main/java/com/example/tmdb/data/Api/TMDbAPI.java
@@ -0,0 +1,50 @@
+package com.example.tmdb.data.Api;
+
+import com.example.tmdb.Dagger.Modules.HttpClientModule;
+import com.example.tmdb.data.models.ResponseCreditDetail;
+import com.example.tmdb.data.models.ResponseMovieDetail;
+import com.example.tmdb.data.models.ResponseNowPlaying;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import rx.Observable;
+
+public interface TMDbAPI {
+
+    String IMAGE_BASE_URL_500 = "https://image.tmdb.org/t/p/w500";
+    String IMAGE_BASE_URL_1280 = "https://image.tmdb.org/t/p/w1280";
+
+    String TMDb_API_KEY = "e2510df5c4693ebfbf806cd9c6900aab";
+
+    @GET(HttpClientModule.NOW_ON_PLAYING)
+    Observable<ResponseNowPlaying> getNowPlaying(
+            @Query("api_key") String api_key,
+            @Query("page") int page
+    );
+
+    @GET(HttpClientModule.POPULAR)
+    Observable<ResponseNowPlaying> getPopularMovie(
+            @Query("api_key") String api_key,
+            @Query("page") int page
+    );
+
+
+    @GET(HttpClientModule.MOVIE_DETAILS + "{movie_id}")
+    Observable<ResponseMovieDetail> getMovieDetail(
+            @Path("movie_id") int movie_id,
+            @Query("api_key") String api_key
+    );
+
+    @GET(HttpClientModule.MOVIE_DETAILS + "{movie_id}/credits")
+    Observable<ResponseCreditDetail> getCreditDetail(
+            @Path("movie_id") int movie_id,
+            @Query("api_key") String api_key
+    );
+
+    @GET(HttpClientModule.MOVIE_DETAILS + "{movie_id}/recommendations")
+    Observable<ResponseNowPlaying> getRecommendDetail(
+            @Path("movie_id") int movie_id,
+            @Query("api_key") String api_key
+    );
+}
