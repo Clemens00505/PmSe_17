81e2ee84deb307c81e241327edf5cf1f590451a8
Josinee <ja.besseling@student.avans.nl>
2024-04-01 14:52:27 +0200

    tablayout laat fragments zien

--- app/src/main/java/com/example/tmdb/MainActivity.java
+++ app/src/main/java/com/example/tmdb/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.tmdb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+
+    private FragmentAdapter fragmentAdapter;
+    private String[] tabLabels = new String[]{"Populair Movies", "My Lists", "Upcoming Movies"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.view_pager);
+        fragmentAdapter = new FragmentAdapter(this);
+        viewPager.setAdapter(fragmentAdapter);
+        tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            tab.setText(tabLabels[position]);
+        }).attach();
+
+    }
+
+
+    private static class FragmentAdapter extends FragmentStateAdapter {
+
+        public FragmentAdapter(FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch(position) {
+                case 0:
+                    return new PopulairMoviesFragment();
+                case 1:
+                    return new ListsFragment();
+                case 2:
+                    return new UpcomingMoviesFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
     }
 }
--- app/src/main/java/com/example/tmdb/PopulairMoviesFragment.java
+++ app/src/main/java/com/example/tmdb/PopulairMoviesFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link populairMoviesFragment#newInstance} factory method to
+ * Use the {@link PopulairMoviesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class populairMoviesFragment extends Fragment {
+public class PopulairMoviesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class populairMoviesFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public populairMoviesFragment() {
+    public PopulairMoviesFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class populairMoviesFragment extends Fragment {
      * @return A new instance of fragment populairMoviesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static populairMoviesFragment newInstance(String param1, String param2) {
-        populairMoviesFragment fragment = new populairMoviesFragment();
+    public static PopulairMoviesFragment newInstance(String param1, String param2) {
+        PopulairMoviesFragment fragment = new PopulairMoviesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
