c66510d24257f7551a87d9fb7629e808e6317b8f
Josinee <ja.besseling@student.avans.nl>
2024-04-02 11:25:47 +0200

    movielistadapter toegevoegd

--- app/src/main/java/com/example/tmdb/MainActivity.java
+++ app/src/main/java/com/example/tmdb/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
